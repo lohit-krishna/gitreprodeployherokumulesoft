d2ecf958653dee2ee457e2f08e9e58e304913231
lohit398 <31948001+lohit398@users.noreply.github.com>
2020-05-18 14:43:33 +0530

    changes made to post deploy script

--- database.js
+++ database.js
@@ -12,21 +12,80 @@ const pool = new Pool({
 
 const client = new pg.Client(connectionString);
 
-client.connect();
-client.query(
-    'CREATE TABLE testpostgresdeploy (id text, Name text, \
-  title TEXT, description TEXT)',
-    (err, res) => {
-        if (err) {
-            return console.error('error with PostgreSQL database', err);
+const checkTableExists = `SELECT
+*
+FROM
+pg_catalog.pg_tables where tablename = 'products'`;
+
+
+const query = `
+				CREATE TABLE products (
+						id int,
+						name varchar,
+						quantity int,
+						price int
+				);
+`;
+
+const queryInsert = `
+INSERT INTO products (id, name, quantity, price)
+VALUES (1, 'Paper Bags', 10, 150);
+INSERT INTO products (id, name, quantity, price)
+VALUES (2, 'Air Filter', 100, 169);
+INSERT INTO products (id, name, quantity, price)
+VALUES (3, 'Exhaust Fan', 10, 1000);
+INSERT INTO products (id, name, quantity, price)
+VALUES (4, 'on Site Technician', 100, 169);
+INSERT INTO products (id, name, quantity, price)
+VALUES (5, 'Motor 1', 10, 50);
+INSERT INTO products (id, name, quantity, price)
+VALUES (6, 'Suite', 10, 269);`;
+
+/*pool
+    .query(checkTableExists)
+    .then((res) => {
+        if (res.rows.length == 0) {
+            pool.query(query).then((res) => {
+                console.log("Table is successfully created");
+                pool.query(queryInsert).then((res) => {
+                    console.log("Data insert successful");
+                });
+            });
         } else {
-            for (let i = 0; i < 10; i++) {
-                pool.query(
-                    "INSERT INTO testpostgresdeploy VALUES ('UA502', 'Lohit', 'MR' ,'I am Lohit Krishna')"
-                );
-            }
+            console.log("Table already Exists!!!");
         }
-        pool.end();
-        client.end();
-    }
-);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+    .finally(() => {
+        //pool.end();
+    });*/
+
+client.connect();
+client.query(checkTableExists)
+    .then((response) => {
+        if (res.rows.length == 0) {
+            client.query(
+                query,
+                (err, res) => {
+                    if (err) {
+                        return console.error('error with PostgreSQL database', err);
+                    } else {
+                        /*for (let i = 0; i < 10; i++) {
+                            pool.query(
+                                "INSERT INTO testpostgresdeploy VALUES ('UA502', 'Lohit', 'MR' ,'I am Lohit Krishna')"
+                            );
+                        }*/
+                        pool.query(queryInsert).then((res) => {
+                            console.log("Data insert successful");
+                        });
+                    }
+                    pool.end();
+                    client.end();
+                }
+            );
+
+        }
+
+    });
