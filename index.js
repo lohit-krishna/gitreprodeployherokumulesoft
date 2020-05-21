69c172bc5977e42c7918aefc0f81baf37ee67183
lohit398 <31948001+lohit398@users.noreply.github.com>
2020-05-22 01:06:37 +0530

    adhere to best practices

--- index.js
+++ index.js
@@ -15,54 +15,6 @@ const pool = new Pool({
 
 pool.connect();
 
-/*const query = `
-				CREATE TABLE products (
-						id int,
-						name varchar,
-						quantity int,
-						price int
-				);
-`;*/
-/*const checkTableExists = `SELECT
-*
-FROM
-pg_catalog.pg_tables where tablename = 'products'`;*/
-
-/*const queryInsert = `
-INSERT INTO products (id, name, quantity, price)
-VALUES (1, 'Paper Bags', 10, 150);
-INSERT INTO products (id, name, quantity, price)
-VALUES (2, 'Air Filter', 100, 169);
-INSERT INTO products (id, name, quantity, price)
-VALUES (3, 'Exhaust Fan', 10, 1000);
-INSERT INTO products (id, name, quantity, price)
-VALUES (4, 'on Site Technician', 100, 169);
-INSERT INTO products (id, name, quantity, price)
-VALUES (5, 'Motor 1', 10, 50);
-INSERT INTO products (id, name, quantity, price)
-VALUES (6, 'Suite', 10, 269);`;*/
-
-/*pool
-    .query(checkTableExists)
-    .then((res) => {
-        if (res.rows.length == 0) {
-            pool.query(query).then((res) => {
-                console.log("Table is successfully created");
-                pool.query(queryInsert).then((res) => {
-                    console.log("Data insert successful");
-                });
-            });
-        } else {
-            console.log("Table already Exists!!!");
-        }
-    })
-    .catch((err) => {
-        console.error(err);
-    })
-    .finally(() => {
-        //pool.end();
-    });*/
-
 var router = express.Router();
 
 router.use(function(req, res, next) {
